5a73145cd226dcaf68b15da6510871fb4e2ef56a
Janet侒然 <oncwnuOBtZwLCmc7UjX_27-FGPEI@git.weixin.qq.com>
2020-12-21 20:54:35 +0800

    修改数据库

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '9b5f34cda84a33a26e3e302b0e77f93b'),

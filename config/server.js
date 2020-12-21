d402d410259901d9fdd1a1b83d1f2b4698064da3
Janet侒然 <oncwnuOBtZwLCmc7UjX_27-FGPEI@git.weixin.qq.com>
2020-12-21 15:43:07 +0800

    blog backmange

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1338),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9b5f34cda84a33a26e3e302b0e77f93b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '94c7053b-0b08-480c-8045-39a265caead1'
+};
